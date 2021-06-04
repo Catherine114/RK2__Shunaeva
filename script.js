4d0d869f7c9252e1a12aebc94ec30b615e29c5e3
Catherine114 <62448379+Catherine114@users.noreply.github.com>
2021-06-04 10:57:32 +0300

    Сделана смена темы, добавление элементов списка

---
+++ script.js
@@ -0,0 +1,70 @@
+Vue.component('task',{
+    props: ['data'],
+    data() {
+      return {
+      }
+    },
+    methods: {
+      task_delete(){
+        this.$emit('task_done')
+      }
+    },
+    template: `
+    <div class="task">
+      <div>
+        <h3 class="task__title">{{data.title}}</h3>
+        <p v-if="data.desc!=''" class="task__desc">{{data.desc}}</p>
+      </div>
+      <button class="task__delete" @click="task_delete()">❌</button>
+    </div>`
+  });
+  
+  var vue = new Vue({
+    el: '#app',
+    data: {
+      string: 'asdasda',
+      new_task: {
+        title: '',
+        desc: ''
+      },
+      tasks : [
+        {
+          title: 'Изучить основы vue.js',
+          desc: 'Попробовать написать калькулятор',
+        },
+        {
+          title: 'Прочитать книгу "Vue.js в действии"',
+          desc: '',
+        },
+        {
+            title: 'Закончить работу над этим сайтом',
+            desc: 'Как можно скорее',
+          }
+      ]
+    },
+    methods: {
+      add_task(){
+        if(this.new_task.title != ''){
+          this.tasks.push({
+            title: this.new_task.title,
+            desc: this.new_task.desc
+          });
+          this.new_task.title='';
+          this.new_task.desc='';
+        }
+      },
+      delete_task(id){
+        this.tasks.splice(id,1);
+      }
+    }
+  });
+
+if (!localStorage.theme) localStorage.theme = 'light'
+document.body.className = localStorage.theme
+themeButton.innerText = document.body.classList.contains("dark") ? "Светлая тема" : "Темная тема"
+
+themeButton.onclick = () => {
+  document.body.classList.toggle("dark")
+  themeButton.innerText = document.body.classList.contains("dark") ? "Светлая тема" : "Темная тема"
+  localStorage.theme = document.body.className || 'light'
+}
